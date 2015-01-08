80503262f2d747dbe8b9b05cfd0c49f5803682ed
Christoph Wurm <cwurm@tibco.com>
2015-01-08 12:28:52 +0100

    Some refactoring

--- Scripts/put.py
+++ Scripts/put.py
@@ -4,21 +4,31 @@ import sys
 import base64
 import requests
 from requests.auth import HTTPBasicAuth
+import os
 import json
 
-headers_get = {'Accept': 'application/repository.file+json'}
+filePath = sys.argv[1]
+fileName = os.path.basename(filePath)
+serverPath = 'http://127.0.0.1:8080/jasperserver-pro/rest_v2/resources/themes/Flat/' + fileName;
+username = 'jasperadmin'
+password = 'jasperadmin'
 
-r = requests.get('http://127.0.0.1:8080/jasperserver-pro/rest_v2/resources/themes/Flat/overrides_custom.css', auth=HTTPBasicAuth('jasperadmin', 'jasperadmin'), headers=headers_get)
+contentType = 'application/repository.file+json'
+httpAuth = HTTPBasicAuth(username, password)
+
+headers_get = {'Accept': contentType}
+
+r = requests.get(serverPath, auth=httpAuth, headers=headers_get)
 data = json.loads(r.text)
 version = data['version']
 
 
-headers_put = {'content-type': 'application/repository.file+json'}
+headers_put = {'content-type': contentType}
 
-f = open(sys.argv[1])
-payload = {'type': 'css', 'content': base64.b64encode(f.read()), 'version': version, 'label': 'css'}
+f = open(filePath)
+payload = {'type': 'css', 'content': base64.b64encode(f.read()), 'version': version, 'label': fileName}
 
-r2 = requests.put('http://127.0.0.1:8080/jasperserver-pro/rest_v2/resources/themes/Flat/overrides_custom.css', data=json.dumps(payload), auth=HTTPBasicAuth('jasperadmin', 'jasperadmin'), headers=headers_put)
+r2 = requests.put(serverPath, data=json.dumps(payload), auth=httpAuth, headers=headers_put)
 
 print r2.text
 
